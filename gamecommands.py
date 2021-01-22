259c73d5c8b33a64963a81ec1a02e8251c213fc6
jae <39254058+sunjaeyoon@users.noreply.github.com>
2021-01-22 11:36:16 -0800

    changed requirements

--- gamecommands.py
+++ gamecommands.py
@@ -6,9 +6,9 @@ import sys
 
 def press(key):
     #if key in keys:
-	pag.keyDown(key)
-	time.sleep(0.000000001)
-	pag.keyUp(key)
+    pag.keyDown(key)
+    time.sleep(0.000000001)
+    pag.keyUp(key)
 
 if __name__ == "__main__":
     press(sys.argv[1])
